8c7418f19b8b01da24a8877da955749f45b27ee8
Sean Anih <sean.anih@gmail.com>
2023-04-01 21:21:27 -0400

    added animations to ball

--- components/three/index.js
+++ components/three/index.js
@@ -4,22 +4,56 @@ import {
 	PerspectiveCamera,
 } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { angleToRadians } from '../../utils/angle';
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 function Three() {
 	const orbitControlRef = useRef(null);
 
-	useFrame(state => {
-		// if (!!orbitControlRef.current) {
-		// 	const { x, y } = state.mouse;
-		// 	console.log(y * angleToRadians(90 - 30));
-		// 	orbitControlRef.current.setAzimuthalAngle(-x * angleToRadians(45));
-		// 	orbitControlRef.current.setPolarAngle((y + 1) * angleToRadians(90 - 30));
-		// 	orbitControlRef.current.update();
-		// }
-	});
+	{
+		/* have camera follow the user's mouse */
+	}
+	// useFrame(state => {
+	// 	if (!!orbitControlRef.current) {
+	// 		const { x, y } = state.mouse;
+	// 		console.log(y * angleToRadians(90 - 30));
+	// 		orbitControlRef.current.setAzimuthalAngle(-x * angleToRadians(45));
+	// 		orbitControlRef.current.setPolarAngle((y + 1) * angleToRadians(90 - 30));
+	// 		orbitControlRef.current.update();
+	// 	}
+	// });
+
+	// Ball Animation
+	const ballRef = useRef(null);
+
+	useEffect(() => {
+		if (!!ballRef.current) {
+			console.log(ballRef.current);
+
+			const timeline = gsap.timeline({ paused: true });
+
+			// gsap for animation
+			// "to" moves geometry from default position to the specified position; "from" starts where specified and ends at default position
+			timeline.to(ballRef.current.position, {
+				x: 1,
+				duration: 2,
+				ease: 'power2.out',
+			});
+			timeline.to(
+				ballRef.current.position,
+				{
+					y: 0.5,
+					duration: 1,
+					ease: 'bounce',
+				},
+				'<'
+			);
+
+			timeline.play();
+		}
+	}, [ballRef.current]);
 
 	return (
 		<>
@@ -31,7 +65,7 @@ function Three() {
 				maxPolarAngle={angleToRadians(80)}
 			/>
 			{/* Ball */}
-			<mesh position={[0, 0.5, 0]} castShadow>
+			<mesh position={[-2, 1.5, 0]} castShadow ref={ballRef}>
 				<sphereGeometry args={[0.5, 32, 32]} />
 				<meshStandardMaterial color='#ffffff' metalness={0.8} roughness={0.2} />
 			</mesh>
@@ -47,7 +81,7 @@ function Three() {
 			{/* spotlight */}
 			<spotLight
 				args={['#ffffff', 1.5, 7, angleToRadians(45), 0.5]}
-				position={[-3, 2, 0]}
+				position={[-2, 2, 0]}
 				castShadow
 			/>
 
